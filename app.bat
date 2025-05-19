496e8c0c289aea27c2957d23be80e72953025f1e
Dejnn <56292588+Dejnn@users.noreply.github.com>
2025-05-19 23:18:04 +0300

    Update app.bat

--- app.bat
+++ app.bat
@@ -1,4 +1,6 @@
-#!/bin/bash
-echo "Hello from My Simple App!"
-echo "Version: 1.0.0"
-echo "Deployment Message: %1"
+@echo off
+REM Це коментар. Перший рядок #!/bin/bash потрібно видалити.
+
+echo Hello from My Simple App (BAT version)!
+echo Version: 1.0.0
+echo Deployment Message: %1
